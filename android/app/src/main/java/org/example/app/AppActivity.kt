bae7a7eedb83fd4edc50b54c14c73b39c83fb70a
valeriy <vlrp512@gmail.com>
2026-03-04 17:56:32 +0500

    Android: host navigation in app activity

--- android/app/src/main/java/org/example/app/AppActivity.kt
+++ android/app/src/main/java/org/example/app/AppActivity.kt
@@ -3,18 +3,11 @@ package org.example.app
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.SystemBarStyle
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.FragmentActivity
-import org.example.android.uikit.theme.AppTheme
-import org.example.app.navigation.RootContainer
-import org.example.library.appRouter.AppRouter
-import org.koin.android.ext.android.inject
 
 class AppActivity : FragmentActivity() {
-
-    private val appRouter: AppRouter by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
@@ -37,12 +30,6 @@ class AppActivity : FragmentActivity() {
             false
         }
 
-        setContent {
-            AppTheme {
-                RootContainer(
-                    destinationRoute = appRouter.getDestination()
-                )
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
