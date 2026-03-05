47f8b49de6b4cf0dfd03f10e755c326ff4bbcfca
valeriy <vlrp512@gmail.com>
2026-03-06 00:36:04 +0500

    iOS: add view controllers

---
+++ ios/App/src/Presentation/RepositoriesListViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class RepositoriesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ ios/App/src/Presentation/RepositoryDetailInfoViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class RepositoryDetailInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
