7fceb25656f95e1f1ec08d443b59ee7928c53a1e
valeriy <vlrp512@gmail.com>
2026-03-14 12:43:12 +0500

    Shared: setup appRouter

--- mpp-library/src/commonMain/kotlin/org/example/library/repositories/domain/AuthDomainRepository.kt
+++ mpp-library/src/commonMain/kotlin/org/example/library/repositories/domain/AuthDomainRepository.kt
@@ -1,12 +1,13 @@
 package org.example.library.repositories.domain
 
-import org.example.library.storage.KeyValueStorage
+import app.xl.gitclientkmp.data.storage.KeyValueStorage
+
 
 internal class AuthDomainRepository(
     private val keyValueStorage: KeyValueStorage,
 ) {
 
     fun isAuthorized(): Boolean {
-        return keyValueStorage.tokens != null
+        return keyValueStorage.getToken() != null
     }
 }
