b0df7b5d42c3ebfd8eb9d76c80b8738a7024f60a
valeriy <vlrp512@gmail.com>
2026-03-15 18:45:08 +0500

    iOS: add IconLabelView with icons

---
+++ ios/App/src/Presentation/Views/IconLabelView.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+final class IconLabelView: UIView {
+    
+    @IBOutlet private weak var iconImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var additionalLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadFromNib()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        loadFromNib()
+    }
+
+    private func loadFromNib() {
+        let bundle = Bundle(for: Self.self)
+        let nib = UINib(nibName: "IconLabelView", bundle: bundle)
+
+        guard let view = nib.instantiate(withOwner: self).first as? UIView
+        else {
+            fatalError("IconLabelView nib not found")
+        }
+
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        addSubview(view)
+    }
+
+    func configure(
+        icon: UIImage?,
+        title: String,
+        titleColor: UIColor,
+        additional: String?,
+    ) {
+        iconImageView.image = icon
+        titleLabel.text = title
+        titleLabel.textColor = titleColor
+        additionalLabel.text = additional ?? ""
+    }
+}
