e533a358f1fff318692b7043d1d239ed4051b1eb
valeriy <vlrp512@gmail.com>
2026-03-16 13:22:47 +0500

    Shared: fix di for iOS

--- mpp-library/src/commonMain/kotlin/org/example/library/di/modules/RepoModule.kt
+++ mpp-library/src/commonMain/kotlin/org/example/library/di/modules/RepoModule.kt
@@ -3,11 +3,16 @@ package org.example.library.di.modules
 import app.xl.gitclientkmp.viewModels.RepositoriesListViewModel
 import app.xl.gitclientkmp.viewModels.RepositoryInfoViewModel
 import org.koin.core.Koin
+import org.koin.core.parameter.parametersOf
 
 fun Koin.getRepositoriesListViewModel(): RepositoriesListViewModel {
     return get()
 }
 
-fun Koin.getRepositoryInfoViewModel(): RepositoryInfoViewModel {
-    return get()
+fun Koin.getRepositoryInfoViewModel(
+    owner: String,
+    repositoryName: String,
+    branch: String
+): RepositoryInfoViewModel {
+    return get { parametersOf(owner, repositoryName, branch) }
 }
