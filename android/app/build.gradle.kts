b6ae240c8a3925d11b72e9dc9764c4f8609c34f2
valeriy <vlrp512@gmail.com>
2026-05-09 18:07:36 +0500

    Android: update markwon factory

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
 
     implementation(projects.mppLibrary)
     implementation(libs.markwon)
+    implementation(libs.markwon.image.coil)
     implementation(libs.moko.units)
     implementation(libs.moko.units.viewbinding)
 
