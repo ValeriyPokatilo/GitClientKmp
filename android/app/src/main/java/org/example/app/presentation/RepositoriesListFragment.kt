dd14781ad82470b88ecb4f8e400f9e1862216d9d
valeriy <vlrp512@gmail.com>
2026-05-13 01:49:31 +0500

    Android: fix build warnings

--- android/app/src/main/java/org/example/app/presentation/RepositoriesListFragment.kt
+++ android/app/src/main/java/org/example/app/presentation/RepositoriesListFragment.kt
@@ -19,7 +19,6 @@ import org.example.app.databinding.RepositoriesListFragmentBinding
 import org.example.app.model.PlaceholderState
 import org.example.app.utils.collectIn
 import org.example.app.utils.toUnitItem
-import org.koin.androidx.viewmodel.ext.android.activityViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RepositoriesListFragment : Fragment() {
